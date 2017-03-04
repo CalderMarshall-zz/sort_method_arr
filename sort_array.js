d3b8149ee33dcf0b8f072ceed25811f7f0c2a9a6
CalderMarshall <cmarshall72@gmail.com>
2017-03-04 18:48:26 -0500

    sort an array 1 written, executed

---
+++ sort_array.js
@@ -0,0 +1,6 @@
+var names = ["lahey", "ricky", "randy", "bubbles", "julian", "cory", "trevor", "lucy"];
+var sorter = function(some_arr){
+     var new_arr = some_arr.sort();
+     console.log(new_arr);
+};
+sorter(names);
